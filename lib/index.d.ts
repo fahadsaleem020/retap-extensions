bd95d3fc95804bcd6e45b9c30964995a19ac852f
palari <fahadsaleem020@gmail.com>
2023-02-10 23:25:43 +0500

    fix(extensions): updated toolbar
    updated toolbar types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,5 +2,5 @@ export { CodeBlockLowlight } from "./extensions/codeblock";
 export { TextIndent } from "./extensions/textindentnode";
 export { FontSize } from "./extensions/fontsizenode";
 export { Youtube } from "./extensions/youtubenode";
-export { Image } from "./extensions/imagenode";
 export { Video } from "./extensions/videonode";
+export { Image } from "./extensions/imagenode";
